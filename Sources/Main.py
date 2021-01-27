49facfacccf25e408af7573da2bb10f7a00925bf
ssw03270 <ttd8591@gmail.com>
2021-01-28 04:01:38 +0900

    [Add] - Add bot setting and commands
    commands : hello and help

---
+++ Sources/Main.py
@@ -0,0 +1,36 @@
+import asyncio,discord
+import os
+from discord.ext import commands
+from discord.ext.commands import bot
+
+# setting bot
+token = "ODA0MDQwODM5ODgxMTYyNzcy.YBGjYg._sqGF9Ts7o0GLfMEI-3Y_d9wvoU"
+game = discord.Game(";sex")
+bot = commands.Bot(command_prefix=';', status=discord.Status.online, activity=game, help_command=None)
+
+# directory of cogs (exttensions)
+os.chdir(os.getcwd() + '/Sources/cogs')
+extensions = ["cogs.Help", "cogs.Hello"]
+
+def main():
+    for ext in extensions:
+        try:
+            bot.load_extension(ext)
+        except Exception as e:
+            print("load fail : " + str(e))
+
+if __name__ == "__main__":
+    main()
+    
+
+# starting bot
+@bot.event
+async def on_ready():
+    print("start bot")
+
+@bot.event
+async def on_member_join(member):
+    await member.send("Hello coding slave. Welcome to BHB.")
+
+bot.run(token)
+
