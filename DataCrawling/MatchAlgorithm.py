8f46225b458d3ef720a2799e78489a1447d13314
ssw03270 <ttd8591@gmail.com>
2021-01-31 02:20:36 +0900

    [Add] - Add url in match_algorithm.json

--- DataCrawling/MatchAlgorithm.py
+++ DataCrawling/MatchAlgorithm.py
@@ -12,13 +12,14 @@ aTags = soup.find("div", {"class": "table-responsive"}).find_all("a")
 # add algorithms in list
 algorithms = []
 for aTag in aTags:
-    algorithms.append(aTag.get_text())
+    
+    algorithms.append([aTag.get_text(), aTag["href"]])
 
 # add algorithms in dict
 dictonary = {}
 for i in range(len(algorithms)):
     if i % 2 == 1:
-        dictonary[algorithms[i]] = algorithms[i - 1]
+        dictonary[algorithms[i][0]] = {'korean':algorithms[i-1][0], 'url':"https://www.acmicpc.net" + algorithms[i-1][1]}
 
 # add algorithms in json file
 with open('match_algorithm.json', 'w', encoding='utf-8') as file :
